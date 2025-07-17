dd733c8a879fd24122bf74513a8a54c666834bac
dandanakaka <43779382+dandanakaka@users.noreply.github.com>
2025-07-17 10:37:57 +0530

    Create RoughMatrix.java
    Using nested for loops, create a pseudo matrix

---
+++ RoughMatrix.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+class HelloWorld2 {
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the rows");
+		int rows = sc.nextInt();
+		System.out.println("Enter the columns");
+		int columns = sc.nextInt();
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0;j<columns; j++) {
+				System.out.println("Enter the elements:");
+				int ele = sc.nextInt();
+				System.out.println(ele);
+				
+
+}
+
+}
+
+	
+
+
+
+
+
+
+
+
+}
+
+}
+
